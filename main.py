18bed905c0853637f02d1175cdf706a2bae3b32c
Kanishk mahor <mahorkanishk@gmail.com>
2021-12-03 22:29:34 +0530

    post

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -12,3 +22,10 @@ def get_user():
 @app.get("/posts")
 def get_posts():
     return {"data: Posts triggered"}
+
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": "new_post"}
